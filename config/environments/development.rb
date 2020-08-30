71f5defb191145a694ff7ce72db68c5369405ebf
Divyanshu Srivastava <harshsrivastava810@gmail.com>
2020-08-30 10:39:51 +0000

    Added home page route as root route and about page route

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.hosts << "69b274b37f1549c59ea0edb7e9de65f0.vfs.cloud9.ap-south-1.amazonaws.com"
 end
